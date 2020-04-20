14e881839054aac69efe029202a34ec3998b6c55
Igor Rudym <irudym@gmail.com>
2020-04-20 18:25:24 +0300

    Fixed ENV variables
    [*] renamed env file

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,3 +1,3 @@
 /* eslint-disable import/prefer-default-export */
-// export const serverUrl = process.env.VUE_APP_ROOT_API;
-export const serverUrl = '//api.ilo-ilo.me';
+export const serverUrl = process.env.VUE_APP_ROOT_API;
+// export const serverUrl = '//api.ilo-ilo.me';
